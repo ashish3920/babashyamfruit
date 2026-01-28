bc575da855d36376574e737ca0c860e2b74e7b64
ashish3920 <ashishgupta08122@gmail.com>
2026-01-28 16:31:55 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@ function toggleMenu() {
     menu.style.display = "flex";
   }
 }
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("show");
+}
+
+document.querySelectorAll('#menu a').forEach(link => {
+  link.addEventListener('click', () => {
+    document.getElementById('menu').classList.remove('show');
+  });
+});
+
+<!-- TOGGLE SCRIPT -->
+
+function toggleBox(id) {
+  const box = document.getElementById(id);
+  box.style.display = box.style.display === "grid" ? "none" : "grid";
+}
